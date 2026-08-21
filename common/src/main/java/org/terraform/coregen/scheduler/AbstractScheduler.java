db61ba72b9b9dcf5c17641bb7d06a4f5a8222357
wickidcow <jaywago@gmail.com>
2026-08-20 21:30:35 -0400

    Add Paper scheduler package

---
+++ common/src/main/java/org/terraform/coregen/scheduler/AbstractScheduler.java
@@ -0,0 +1,13 @@
+package org.terraform.coregen.scheduler;
+
+import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
+
+public interface AbstractScheduler {
+
+    void execAsyncRegion(@NotNull World world, int chunkX, int chunkZ, @NotNull Runnable run);
+
+    void execSyncGlobal(@NotNull Runnable run);
+
+    void execAsync(@NotNull Runnable run);
+}

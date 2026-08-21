315080116f6003f48fafcfc7689cdb2d9a8e0ca7
wickidcow <jaywago@gmail.com>
2026-08-20 21:23:48 -0400

    Restore explicit common compile dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,6 +3,8 @@ group = "org.terraform"
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.2.build.+")
     compileOnly("org.jetbrains:annotations:20.1.0")
+    compileOnly("com.google.code.findbugs:jsr305:3.0.2")
+    compileOnly("commons-lang:commons-lang:2.6")
     compileOnly("com.github.AvarionMC:yaml:1.1.7")
 }
 
